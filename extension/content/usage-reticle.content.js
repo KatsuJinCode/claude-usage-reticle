77ece8af0d98d4defe06fee1db61d766bbe0dcf4
Julian <jpswilner@gmail.com>
2026-05-28 16:28:29 -0400

    post-edit: usage-reticle.content.js

--- extension/content/usage-reticle.content.js
+++ extension/content/usage-reticle.content.js
@@ -1156,8 +1156,6 @@
     function findResetBlock(bar) {
         var node = bar.parentElement;
         for (var depth = 0; depth < 10 && node; depth++) {
-            node = node.parentElement;
-            if (!node) break;
             var nodes = node.querySelectorAll('span, div, p, small, font');
             for (var i = 0; i < nodes.length; i++) {
                 var nc = (nodes[i].textContent || '').replace(/\s+/g, ' ').trim();
@@ -1165,6 +1163,7 @@
                     return {block: node, resetEl: nodes[i]};
                 }
             }
+            node = node.parentElement;
         }
         return null;
     }
